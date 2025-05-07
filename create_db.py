4fdaf45b072de039888b589d55c35ac4a1820b22
mswed <mswed@beapot.com>
2025-05-06 22:00:04 -0400

    added db creation script

---
+++ create_db.py
@@ -0,0 +1,12 @@
+from models import db
+from app import create_app
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+test_app = create_app('wikmt_test_db')
+
+with test_app.app_context():
+    db.create_all()
