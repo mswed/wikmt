02bdfc83ba7e85f344ae352c7565717bfbb009e4
mswed <mswed@beapot.com>
2025-05-06 20:48:10 -0400

    very strange still

--- app.py
+++ app.py
@@ -527,5 +527,5 @@ def hello():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080)
